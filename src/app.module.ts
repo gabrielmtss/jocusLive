7f4ef9a9a582f394b0649b543c26f67c18dd4818
Gabriel Matos <g.matos@outlook.com>
2022-05-18 23:39:38 -0300

    add games and genders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GamesModule } from './games/games.module';
+import { GendersModule } from './genders/genders.module';
 
 @Module({
-  imports: [],
+  imports: [GamesModule, GendersModule],
   controllers: [AppController],
   providers: [AppService],
 })
