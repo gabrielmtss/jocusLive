748a0678108c33ba50221ec77c98272b57891cf2
Gabriel Matos <g.matos@outlook.com>
2022-05-19 22:02:44 -0300

    add findAll and create methods

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { GamesService } from './games.service';
 
 @Controller('games')
-export class GamesController {}
+export class GamesController {
+  constructor(private gamesService: GamesService) {}
+
+  @Get()
+  findAll() {
+    return this.gamesService.findAll();
+  }
+
+  @Post()
+  create() {
+    return this.gamesService.create();
+  }
+}
--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class GamesService {}
+export class GamesService {
+  findAll() {
+    return 'Listar todas as mesas';
+  }
+
+  create() {
+    return 'Adicionar um jogo';
+  }
+}
