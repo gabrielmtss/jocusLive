8e48c88c5f4cd32ed8da93dc3ca77a814b8521c2
Gabriel Matos <g.matos@outlook.com>
2022-05-22 13:00:31 -0300

    feat(genders) create methods

--- src/genders/genders.controller.ts
+++ src/genders/genders.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('genders')
-export class GendersController {}
+export class GendersController {
+  gendersService: any;
+  @Get()
+  findAll() {
+    return this.gendersService.findAll();
+  }
+
+  @Get()
+  findById() {
+    return this.gendersService.findById();
+  }
+
+  @Post()
+  create() {
+    return this.gendersService.create();
+  }
+
+  @Put()
+  update() {
+    return this.gendersService.update();
+  }
+
+  @Delete()
+  delete() {
+    return this.gendersService.delete();
+  }
+}
