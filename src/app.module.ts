3bdab16ec32c4f64c873cb1a2cf4515009e3d296
Gabriel Matos <g.matos@outlook.com>
2022-05-30 21:50:47 -0300

    create profiles and users entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { GamesModule } from './games/games.module';
 import { GendersModule } from './genders/genders.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
-  imports: [GamesModule, GendersModule, PrismaModule],
+  imports: [GamesModule, GendersModule, PrismaModule, UsersModule, ProfilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
